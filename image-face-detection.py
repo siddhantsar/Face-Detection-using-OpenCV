22e3ba2cb43afc50905611b1ac775d2539d72d02
siddhantsar <51258285+siddhantsar@users.noreply.github.com>
2018-06-28 17:47:28 +0530

    Commit 28-06-2018

--- image-face-detection.py
+++ image-face-detection.py
@@ -1,10 +1,11 @@
 import cv2
+import csv
 
 if __name__ == "__main__":
 	# Loading the cascade XML file using CascadeClassifier method. 
 	face_cascades = cv2.CascadeClassifier("xml-files/haarcascade_frontalface_alt.xml")
 
-	img = cv2.imread("images-faces/image-group-1.jpg")
+	img = cv2.imread("images/image-group-1.jpg")
 	img = cv2.resize(img, None, fx=0.1, fy=0.1, interpolation = cv2.INTER_CUBIC)
 
 	# For storing x, y pixels of the face in any given image
